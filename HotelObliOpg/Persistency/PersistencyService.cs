d5d3dd2897c8bab89e4741448a6efa0bf509e014
Matias <Matias@DESKTOP-TA0TGEP>
2017-03-29 14:29:34 +0200

    added kode i persistency (ikke færdig) og added viewmodel (ikke færdig)

---
+++ HotelObliOpg/Persistency/PersistencyService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HotelObliOpg.Model;
+using System.Collections;
+
+namespace HotelObliOpg.Persistency
+{
+    class PersistencyService
+    {
+
+        public static async Task<ObservableCollection<Guest>> loadGuestAsync()
+        {
+            const string serverUrl = "http://localhost:10723/";
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(serverUrl);
+                client.DefaultRequestHeaders.Clear();
+                string urlString = "api/Guest";
+                try
+                {
+                    HttpResponseMessage response = client.GetAsync(urlString).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var guestlist = response.Content.ReadAsAsync<List<Guest>>().Result;
+                        foreach (var guest in guestlist)
+                        {
+                            
+                        }
+
+                    }
+                }
+                catch (Exception e)
+                {
+
+
+                }
+            }
+        }
+    }
+}
---
+++ HotelObliOpg/ViewModel/GuestViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelObliOpg.ViewModel
+{
+    class GuestViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public int Guest_No { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+            
+    }
+}
