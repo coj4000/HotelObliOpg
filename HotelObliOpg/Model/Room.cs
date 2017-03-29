2315faf0a72fadb38cc294efd57bc6299302e5a4
Matias <Matias@DESKTOP-TA0TGEP>
2017-03-29 12:51:05 +0200

    opretter classes (booking, hotel, guest, room)

---
+++ HotelObliOpg/Model/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelObliOpg.Model
+{
+    class Room
+    {
+        public int Room_No { get; set; }
+
+        public int Hotel_No { get; set; }
+
+        public string Types { get; set; }
+
+        public double? Price { get; set; }
+    }
+}
