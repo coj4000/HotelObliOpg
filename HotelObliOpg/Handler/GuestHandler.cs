ae8604127d177cee3294c56e136c6ad7109c4091
Christian <Christian@WORKSTATION>
2017-03-30 11:19:20 +0200

    har lavet lille del af handler og lidt af commands til create, update og delete

---
+++ HotelObliOpg/Handler/GuestHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelObliOpg.ViewModel;
+
+namespace HotelObliOpg.Handler
+{
+    class GuestHandler
+    {
+        public GuestViewModel GuestViewModel { get; set; }
+
+        public GuestHandler(GuestViewModel gvm)
+        {
+            this.GuestViewModel = gvm;
+        }
+
+        public CreateGuest()
+        {
+            Guest tempGuest = new Guest(GuestViewModel.Guest_No, GuestViewModel.Name, GuestViewModel.Address);
+        }
+    }
+}
--- HotelObliOpg/ViewModel/GuestViewModel.cs
+++ HotelObliOpg/ViewModel/GuestViewModel.cs
@@ -4,18 +4,29 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using HotelObliOpg.Common;
 
 namespace HotelObliOpg.ViewModel
 {
     class GuestViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand CreateGuestCommand { get; set; }
+        public ICommand DeleteGuestCommand { get; set; }
+        public ICommand UpdateGuestCommand { get; set; }
 
 
         public int Guest_No { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
 
+        public GuestViewModel()
+        {
+            CreateGuestCommand = new RelayCommand(CreateGuest, null);
+            DeleteGuestCommand = new RelayCommand(DeleteGuest, CanDeleteGuest);
+            UpdateGuestCommand = new RelayCommand(UpdateGuest, CanUpdateGuest);
+        }
             
     }
 }
