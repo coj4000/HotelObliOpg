15413cf628f2251163e740e08d04701c25a1de37
Matias <Matias@DESKTOP-TA0TGEP>
2017-04-02 21:00:48 +0200

    oprettet skabelon for singleton

---
+++ HotelObliOpg/Model/GuestCatalogSingleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelObliOpg.Model
+{
+    class GuestCatalogSingleton
+    {
+        private ObservableCollection<Guest> guests;
+
+        public ObservableCollection<Guest> Guests
+        {
+            get { return guests; }
+            set { guests = value; }
+
+        }
+
+        private static GuestCatalogSingleton instance;
+
+        public static GuestCatalogSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new GuestCatalogSingleton();
+                }
+                return instance;
+            }
+
+        }
+
+
+    }
+}
