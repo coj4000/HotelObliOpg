23e8d3f1d9e2e7c71d58fa227d5d34fb5713a1ca
Christian <Christian@WORKSTATION>
2017-04-07 12:50:13 +0200

    Har lavet get test

---
+++ HotelObliTest/Guest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelObliTest
+{
+    class Guest
+    {
+        public int Guest_No { get; set; }
+
+
+        public string Name { get; set; }
+
+
+        public string Address { get; set; }
+
+        public Guest()
+        {
+            
+        }
+
+        
+    }
+}
